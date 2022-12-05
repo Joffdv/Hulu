37d0e8c90471a32eb2404044f272e2d1e95ddf93
João Emanuel <107960713+Joffdv@users.noreply.github.com>
2022-12-05 13:51:52 +0000

    Initial commit

--- hulu-clone/components/Header.tsx
+++ hulu-clone/components/Header.tsx
@@ -1,12 +1,28 @@
+import { 
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
 import Image from 'next/image'
+import HeaderItem from '../components/HeaderItem'
 
 export default function Header() {
   return (
-    <header>
-      <h1>header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
       <Image
         className="object-contain"
-        src="hulu-white.png"
+        src="/hulu-white.png"
         width={200}
         height={100}
       />
--- hulu-clone/pages/index.tsx
+++ hulu-clone/pages/index.tsx
@@ -1,16 +1,37 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
-
-export default function Home() {
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
+//Client render
+export default function Home({ results }) {
   return (
-    <div className="">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
+      <Nav />
+      <Results results={results} />
     </div>
   )
+}
+
+//Server side render
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  }
 }
